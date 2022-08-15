baa86b078f187f0368cc3fa11b51e3a86272d77b
Nicholas Poulidis <nicholas.poulidis@gmail.com>
2022-08-15 09:10:15 -0400

    chore: adding docstrings for addLowCard

--- src/domains/runningCount.py
+++ src/domains/runningCount.py
@@ -1,5 +1,3 @@
-
-
 class runningCount:
     def __init__(self):
         self._total = 0
@@ -8,6 +6,6 @@ class runningCount:
     def total(self):
         return self._total
 
-    
     def addLowCard(self):
+        """Adds 1 to the count for any card with face value from 2-6"""
         self._total += 1
