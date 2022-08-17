d0d6512e764c596a06a3eb728bda903af99a1cd0
Nicholas Poulidis <nicholas.poulidis@gmail.com>
2022-08-17 08:48:33 -0400

    refactor: running count total will be passed into true count object instead of running count object

--- tests/test_true_count.py
+++ tests/test_true_count.py
@@ -3,14 +3,12 @@ from pytest import approx
 
 
 def test_gets_true_count_with_running_count_of_zero():
-    running_count = RunningCount()
-    true_count = TrueCount(running_count, 6)
+    true_count = TrueCount(0, 6)
 
     assert true_count.total == 0
 
 
 def test_gets_true_count_with_running_count_not_zero():
-    running_count = RunningCount()
     true_count = TrueCount(8, 6)
 
     assert approx(true_count.total, 0.1) == 1.3
