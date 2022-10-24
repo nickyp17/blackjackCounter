0f077a8b1214a8b73446ffea0146dd5cb16cdb0f
Nicholas Poulidis <71410380+nickyp17@users.noreply.github.com>
2022-10-24 10:43:32 -0400

    refactor: maintain encapsulation by making true count use the running count object

--- src/card_counter.py
+++ src/card_counter.py
@@ -1,21 +1,3 @@
-class TrueCount:
-    def __init__(self, running_count, number_of_decks):
-        self._running_count = running_count
-        self._number_of_decks = number_of_decks
-
-    @property
-    def total(self):
-        return self._running_count.total / self._number_of_decks
-
-    @property
-    def number_of_decks(self):
-        return self._number_of_decks
-
-    def card_pulled(self):
-        number_of_cards = self._number_of_decks * 52 - 1
-        self._number_of_decks = number_of_cards / 52
-
-
 class RunningCount:
     def __init__(self, total=0):
         self._total = total
@@ -31,3 +13,29 @@ class RunningCount:
     def add_high_card(self):
         """Adds -1 to the count for any card with face value from 10-Ace"""
         self._total = -1
+
+
+class TrueCount:
+    def __init__(self, number_of_decks):
+        self._running_count = RunningCount()
+        self._number_of_decks = number_of_decks
+
+    @property
+    def total(self):
+        return self._running_count.total / self._number_of_decks
+
+    @property
+    def number_of_decks(self):
+        return self._number_of_decks
+
+    def _update_number_of_decks(self):
+        number_of_cards = self._number_of_decks * 52 - 1
+        self._number_of_decks = number_of_cards / 52
+
+    def low_card_pulled(self):
+        self._running_count.add_low_card()
+        self._update_number_of_decks()
+
+    def high_card_pulled(self):
+        self._running_count.add_high_card()
+        self._update_number_of_decks()
--- tests/test_true_count.py
+++ tests/test_true_count.py
@@ -3,35 +3,31 @@ from pytest import approx
 
 
 def test_gets_true_count_with_running_count_of_zero():
-    running_count = RunningCount()
-    true_count = TrueCount(running_count, 6)
+    true_count = TrueCount(6)
 
     assert true_count.total == 0
 
 
 def test_gets_true_count_with_running_count_not_zero():
-    running_count = RunningCount()
+    true_count = TrueCount(6)
     for i in range(8):
-        running_count.add_low_card()
-    true_count = TrueCount(running_count, 6)
+        true_count.low_card_pulled()
 
     assert approx(true_count.total, 0.1) == 1.3
 
 
 def test_adjusts_deck_number_when_card_pulled():
-    running_count = RunningCount()
-    true_count = TrueCount(running_count, 6)
+    true_count = TrueCount(6)
 
-    true_count.card_pulled()
+    true_count.high_card_pulled()
 
     assert approx(true_count.number_of_decks, 0.001) == 5.98
 
 
 def test_adjusts_deck_number_when_two_cards_pulled():
-    running_count = RunningCount()
-    true_count = TrueCount(running_count, 6)
+    true_count = TrueCount(6)
 
-    true_count.card_pulled()
-    true_count.card_pulled()
+    true_count.low_card_pulled()
+    true_count.high_card_pulled()
 
     assert approx(true_count.number_of_decks, 0.001) == 5.96
